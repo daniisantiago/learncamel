bad6e592744c0c68690e47d0e62fbf818601092a
Daniella Santiago <danii.santiago@gmail.com>
2020-05-24 18:54:24 -0300

    unmarshal com fixed length no arquivo

---
+++ src/test/java/com/learncamel/routes/fixedLength/FixedLengthUnMarshalRouteTest.java
@@ -0,0 +1,28 @@
+package com.learncamel.routes.fixedLength;
+
+import com.learncamel.domain.EmployeeWithFixedLength;
+import org.apache.camel.Exchange;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+import java.util.List;
+
+public class FixedLengthUnMarshalRouteTest extends CamelTestSupport {
+    @Override
+    protected RoutesBuilder createRouteBuilder() throws Exception {
+        return new FixedLengthUnMarshalRoute();
+    }
+
+    @Test
+    public void unMarshalFixedLenghtTest() throws InterruptedException {
+
+        Exchange exchange = consumer.receive("direct:output");
+        List<EmployeeWithFixedLength> employeeWithFixedLengthsList = (List<EmployeeWithFixedLength>) exchange.getIn().getBody();
+        //Thread.sleep(5000);
+
+        assertNotNull(employeeWithFixedLengthsList);
+        assertEquals("dani", employeeWithFixedLengthsList.get(0).getName());
+        assertEquals("Engineer", employeeWithFixedLengthsList.get(1).getRole());
+    }
+}
