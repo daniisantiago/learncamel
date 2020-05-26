8ad803458cc11c8d1c2cc8ac5493d9b08394069d
Daniella Santiago <danii.santiago@gmail.com>
2020-05-25 21:59:55 -0300

    criacao do projeto activemq2db, porem n consegui enviar uma msg

---
+++ activemq2db/src/main/java/com/learncamel/jms/JmsReadRoute.java
@@ -0,0 +1,11 @@
+package com.learncamel.jms;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class JmsReadRoute extends RouteBuilder {
+    public void configure() throws Exception {
+        from("activemq:queue:testQueue")
+                .to("log:?level=INFO&showBody=true")
+                .to("direct:readQueue");
+    }
+}
---
+++ activemq2db/src/test/java/com/learncamel/jms/JmsReadRouteTest.java
@@ -0,0 +1,21 @@
+package com.learncamel.jms;
+
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+public class JmsReadRouteTest extends CamelTestSupport {
+    @Override
+    protected RoutesBuilder createRouteBuilder() throws Exception {
+        return new JmsReadRoute();
+    }
+
+    @Test
+    public void readMessageFromActiveMQ() throws InterruptedException{
+
+        String expected = "123";
+        String response = consumer.receiveBody("direct:readQueue", String.class);
+        System.out.println("The response is : " + response);
+        assertEquals(expected,response);
+    }
+}
